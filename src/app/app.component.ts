51bdd42b4003670911a246ebf212520749fa8086
tony2tones <anthonymarques@outlook.com>
2021-06-15 13:28:21 +0200

    fixed the input outpu problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from './models/card.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'input-output';
+  userProfile: Card;
+
+  data = {
+    name: 'Jonny Doe',
+    job: 'FrontEnd Gopher',
+    bio: 'I am a Gopher that likes to FrontEnd Gophe',
+  };
+
+  ngOnInit() {
+    this.userProfile = this.data;
+  }
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from 'src/app/models/card.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  // cardprofile:Card;
+  @Input() name: String;
+  @Input() job: String;
+  @Input() bio: String;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // name = 'Jonny Doe';
+  // job = 'FrontEnd Gopher';
+  // bio = 'I am a Gopher that likes to FrontEnd Gophe';
 }
