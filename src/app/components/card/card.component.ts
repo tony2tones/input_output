6e13e9773e4cc32493791b8a31eb89d7ddd644c4
tony2tones <anthonymarques@outlook.com>
2021-06-26 12:03:29 +0200

    feat(styles sort of sorted and added follow button

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Card } from 'src/app/models/card.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class CardComponent {
   @Input() name: String;
