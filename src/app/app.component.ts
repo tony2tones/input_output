aecc4b949200516181511c365d776ce965e944a9
tony2tones <anthonymarques@outlook.com>
2021-08-31 16:35:16 +0200

    working on styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,29 +11,29 @@ export class AppComponent implements OnInit {
   userProfiles: Card[] = [];
   // userProfiles:Card;
 
-  data = [{
-        name: 'Jonny Doe',
-        job: 'FrontEnd Gopher',
-        bio: 'I am a Gopher that likes to FrontEnd Gophe',
-      }]
+  // data = [{
+  //       name: 'Jonny Doe',
+  //       job: 'FrontEnd Gopher',
+  //       bio: 'I am a Gopher that likes to FrontEnd Gophe',
+  //     }]
 
-  // data = [
-  //   {
-  //     name: 'Jonny Doe',
-  //     job: 'FrontEnd Gopher',
-  //     bio: 'I am a Gopher that likes to FrontEnd Gophe',
-  //   },
-  //   {
-  //     name: 'John Dear',
-  //     job: 'Dear FrontEnd',
-  //     bio: 'I am a Dear that likes to dearly FrontEnd',
-  //   },
-  //   {
-  //     name: 'Tony Tones',
-  //     job: 'FrontEnd Developer',
-  //     bio: 'I am a Tony Tones, enjoys footy and I use JavaScript, TypeScript and Angular Framework',
-  //   },
-  // ];
+  data = [
+    {
+      name: 'Jonny Doe',
+      job: 'FrontEnd Gopher',
+      bio: 'I am a Gopher that likes to FrontEnd Gophe',
+    },
+    {
+      name: 'John Dear',
+      job: 'Dear FrontEnd',
+      bio: 'I am a Dear that likes to dearly FrontEnd',
+    },
+    {
+      name: 'Tony Tones',
+      job: 'FrontEnd Developer',
+      bio: 'I am a Tony Tones, enjoys footy and I use JavaScript, TypeScript and Angular Framework',
+    },
+  ];
 
   ngOnInit() {
     this.userProfiles = this.data;
